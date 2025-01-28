61c4ce7cc2f667fdd8bcc6add74db15b4019ad63
shristishetty <shristishetty67@gmail.com>
2025-01-28 18:08:38 +0530

    Login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,71 +1,3 @@
-// import { useState } from "react";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-// import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-// import axios from 'axios';
-
-// interface LoginProps {
-//   onLoginSuccess: () => void;
-// }
-
-// function Login({ onLoginSuccess }: LoginProps) {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState("");
-
-//   const handleLogin = async () => {
-//     try {
-//       const response = await axios.post("http://localhost:5000/login", {
-//         username,
-//         password
-//       });
-
-//       if (response.status === 200) {
-//         setError("");
-//         onLoginSuccess(); // Call the success callback if login is successful
-//       }
-//     } catch (err: any) {
-//       setError(err.response?.data?.message || "Something went wrong");
-//     }
-//   };
-
-//   return (
-//     <div className="min-h-screen flex fle-col items-center justify-center py-6 px-4">
-      
-//       <Card className="w-96 shadow-lg">
-//         <CardHeader>
-//           <CardTitle className="text-center text-xl font-bold">Login</CardTitle>
-//         </CardHeader>
-//         <CardContent>
-//           <div className="flex flex-col gap-4">
-//             <Input
-//               type="text"
-//               placeholder="Username"
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//             <Input
-//               type="password"
-//               placeholder="Password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             {error && <p className="text-red-500 text-sm">{error}</p>}
-//           </div>
-//         </CardContent>
-//         <CardFooter>
-//           <Button className="w-full" onClick={handleLogin}>
-//             Login
-//           </Button>
-//         </CardFooter>
-//       </Card>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
-
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -83,7 +15,7 @@ function Login({ onLoginSuccess }: LoginProps) {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/login", {
+      const response = await axios.post("https://newsroom-analytics-demo.vercel.app/login", {
         username,
         password
       });
